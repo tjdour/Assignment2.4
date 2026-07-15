f0a0a89c3782ce16e863a15e15de7f35f95392eb
tjdour <tongjdour@outlook.com>
2026-07-15 20:20:54 +0300

    Assignment 2.4.2

---
+++ Assignment2.4.2/Program.cs
@@ -0,0 +1,44 @@
+﻿//Assignmetn 2.4.2
+Console.WriteLine("Find Largest of 3 numbers");
+Console.WriteLine("Enter first number: ");
+int first = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter second number: ");
+int second = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter third number: ");
+int third = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"You entered the numbers: {first}, {second}, {third}");
+
+int largest = first;
+string pos = "1st";
+
+if (second > largest)
+{
+    largest = second;
+    pos = "2nd";
+}
+
+if (third > largest)
+{ 
+    largest=third;
+    pos = "3rd";
+}
+
+Console.WriteLine($"The {pos} Number is the greatest among three: {largest}");
+
+
+
+//if (first >= second && first >= third)
+//{
+//    Console.WriteLine("First largest");
+//}
+//else if (second >= first && second >= third)
+//{
+//    Console.WriteLine("Second is largest");
+//}
+//else
+//{ 
+//    Console.WriteLine("Third is largest");
+//}
