59881d4394e272656e42344426ffb6288e996ed4
tjdour <tongjdour@outlook.com>
2026-07-15 18:54:44 +0300

    Add project files.

---
+++ Assignment2.4.1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+//Assignment 2.4.1
+Console.WriteLine("Calculating sum of an array.\nEnter size of array: ");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int [arraySize];
+
+Console.WriteLine($"Enter {arraySize} elements in the array:");
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write($"Element - {i}: ");
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int sum = 0;
+foreach (int n in numbers)
+{
+    sum += n;
+}
+Console.WriteLine($"The sum of the array is: {sum}");
